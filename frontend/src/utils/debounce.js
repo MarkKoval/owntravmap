dddfc3c27343ace5d7d43a9709de3aaa42f93217
Mark Koval <marekmark22@gmail.com>
2026-01-27 14:17:16 +0200

    ghjk

--- frontend/src/utils/debounce.js
+++
@@ -1,7 +0,0 @@
-export function debounce(fn, delay = 200) {
-  let timer;
-  return (...args) => {
-    if (timer) window.clearTimeout(timer);
-    timer = window.setTimeout(() => fn(...args), delay);
-  };
-}
